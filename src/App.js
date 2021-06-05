98a54b9b1cf7d1970da1298043d5de6c4e22f045
Ashish-kumar-pradhan <80833940+Ashish-kumar-pradhan@users.noreply.github.com>
2021-06-05 13:33:43 +0530

    starting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>YO</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+    <Switch>
+      <Route path = "/" exact />
+    </Switch>
+    </Router>
+    </>
   );
 }
 
