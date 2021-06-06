ca8f9e715694347cc23f87c1005dc61ac0ae679c
Ashish-kumar-pradhan <80833940+Ashish-kumar-pradhan@users.noreply.github.com>
2021-06-06 14:10:44 +0530

    Adding buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,20 +26,24 @@ function Navbar() {
                            </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link 
-                            to='/services' 
-                            className='nav-links' 
-                            onClick={closeMobileMenu}>
+                            <Link
+                                to='/services'
+                                className='nav-links'
+                                onClick={closeMobileMenu}>
                                 Services
                            </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            <Link to='/products'
+                                className='nav-links'
+                                onClick={closeMobileMenu}>
                                 Products
                            </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            <Link to='/sign-up'
+                                className='nav-links-mobile'
+                                onClick={closeMobileMenu}>
                                 Sign Up
                            </Link>
                         </li>
